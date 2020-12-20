15862367589cef45cee52c9ea442170b08520d9e
Ana_stasya_S <anastasiya-s@live.de>
2020-12-20 14:42:17 +0300

    feat: show flag

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import handleRenderingAllCountriesList from './countries-list/countriesList';
 
 let countriesList = [];
 let countriesActiveProp = 'totalConfirmed';
-let countriesDetails = [];
+const countriesDetails = [];
 
 const sortCountriesByParam = (field) => function innerSort(a, b) {
   let compareResult = null;
@@ -21,48 +21,50 @@ const sortCountriesByParam = (field) => function innerSort(a, b) {
   return compareResult;
 };
 
-const fetchFlagAndPopulation = async () => {
-  const response = await fetch('https://restcountries.eu/rest/v2/all?fields=name;population;flag');
-  const data = await response.json();
-  countriesDetails = data;
-  return countriesDetails;
-};
+// const fetchFlagAndPopulation = async () => {
+//   const response = await fetch('https://restcountries.eu/rest/v2/all?fields=name;population;flag');
+//   const data = await response.json();
+//   data.forEach((details) => countriesDetails.push(details));
+//   return countriesDetails;
+// };
 
-const handleFetchingData = (currentParam) => {
+const handleFetchingData = async (currentParam) => {
   const param = currentParam;
-  fetch(fetchSummary).then((response) => response.json()).then((data) => {
-    countriesList = data.Countries.map(({
-      Country,
-      Slug,
-      CountryCode,
-      NewConfirmed,
-      NewDeaths,
-      NewRecovered,
-      TotalConfirmed,
-      TotalDeaths,
-      TotalRecovered,
-    }) => {
-      const item = {};
-      item.country = Country;
-      item.slug = Slug;
-      item.countryCode = CountryCode;
-      item.newConfirmed = NewConfirmed;
-      item.newDeaths = NewDeaths;
-      item.newRecovered = NewRecovered;
-      item.totalConfirmed = TotalConfirmed;
-      item.totalDeaths = TotalDeaths;
-      item.totalRecovered = TotalRecovered;
-      return item;
-    });
-    countriesList.sort(sortCountriesByParam(param));
-    handleRenderingAllCountriesList(countriesList, param, countriesDetails);
-    return countriesList;
+  const response = await fetch(fetchSummary);
+  const data = await response.json();
+  const detailsResponse = await fetch('https://restcountries.eu/rest/v2/all?fields=name;population;flag');
+  const detailsData = await detailsResponse.json();
+  detailsData.forEach((details) => countriesDetails.push(details));
+  countriesList = data.Countries.map(({
+    Country,
+    Slug,
+    CountryCode,
+    NewConfirmed,
+    NewDeaths,
+    NewRecovered,
+    TotalConfirmed,
+    TotalDeaths,
+    TotalRecovered,
+  }) => {
+    const item = {};
+    item.country = Country;
+    item.slug = Slug;
+    item.countryCode = CountryCode;
+    item.newConfirmed = NewConfirmed;
+    item.newDeaths = NewDeaths;
+    item.newRecovered = NewRecovered;
+    item.totalConfirmed = TotalConfirmed;
+    item.totalDeaths = TotalDeaths;
+    item.totalRecovered = TotalRecovered;
+    return item;
   });
+  countriesList.sort(sortCountriesByParam(param));
+  handleRenderingAllCountriesList(countriesList, param, countriesDetails);
+  return countriesList;
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   const dataParams = document.querySelectorAll('.data-panel');
-  fetchFlagAndPopulation();
   handleFetchingData(countriesActiveProp);
   dataParams.forEach((item) => item.addEventListener('click', (e) => {
     countriesActiveProp = e.target.dataset.info;
